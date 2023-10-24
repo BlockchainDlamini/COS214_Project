70fbac99b0e8d84cee7f416aa7114a0b2155781c
ArisNeimanRose <129956034+ArisNeimanRose@users.noreply.github.com>
2023-10-24 20:14:31 +0200

    Update OrderProcessState.h
    Added constructor and adjustment to handleChange function

--- .code/OrderProcessState.h
+++ .code/OrderProcessState.h
@@ -1,15 +1,16 @@
 #ifndef OrderProcessState_H
 #define OrderProcessState_H
 #include "Customer.h"
-class Customer;
+
 class OrderProcessState
 {
 private:
     std::shared_ptr<Customer> context;
 
 public:
+    OrderProcessState(){};
     virtual void execute();
-    virtual void handleChange();
+    virtual void handleChange(std::shared_ptr<Customer>);
     virtual std::shared_ptr<Customer> getState();
 };
 #endif
