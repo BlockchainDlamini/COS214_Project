07cab02f6908cfc3f6aa619acff2a9dd810ce413
ArisNeimanRose <129956034+ArisNeimanRose@users.noreply.github.com>
2023-10-24 20:11:58 +0200

    Update Customer.h
    Make some adjustments to some of the functions, since some of them needed parameters and added constructors

--- .code/Customer.h
+++ .code/Customer.h
@@ -2,29 +2,60 @@
 #define Customer_H
 #include "EmotionState.h"
 #include "OrderProcessState.h"
+#include "Happy.h"
+#include "Ordering.h"
+#include "Preorder.h"
+#include "Waiting.h"
+#include "Satisfied.h"
+#include "Disgruntaled.h"
+#include "DinnerDone.h"
+#include "Disgruntaled.h"
+#include "OrderReceived.h"
 #include "Order.h"
+#include "Tab.h"
 #include <iostream>
 #include <memory>
 #include <vector>
-class Customer
+using namespace std;
+class OrderProcessState;
+class EmotionState;
+class Customer : public enable_shared_from_this<Customer>
 {
 private:
+    int ID;
     std::shared_ptr<EmotionState> mood;
     std::shared_ptr<OrderProcessState> orderProcess;
-    bool hasOrdered;
+    int bankAccountAmount;
+    std::shared_ptr<Tab> tab;
+    int tableNum;
     std::vector<std::shared_ptr<Order>> orders;
 
 public:
     Customer();
+    Customer(std::shared_ptr<EmotionState>, int);
     void get();
     void changed();
     void set();
+    void changeMood();
+    void changedOrderProcessState();
+    void setID(int);
     void requestBill();
     bool payBill(char c);
     bool isLoyal();
+    void startTab();
+    void createOrder(std::shared_ptr<Order> order);
     void beSeated(int tableNum);
     int getTableNum();
+    void setEmotionState(std::shared_ptr<EmotionState> mood);
+    void setOrderProcessState(std::shared_ptr<OrderProcessState> orderProcess);
+    std::shared_ptr<EmotionState> getMood();
+    std::shared_ptr<OrderProcessState> getOrderProcessState();
     std::vector<std::shared_ptr<Order>> sendOrder();
-    bool receiveOrder();
+    bool receiveOrder(std::vector<std::shared_ptr<Order>>);
+    std::string printCustomer();
+    bool hasOrdered;
+    bool hasBill;
+    bool hasFood = false;
+    bool has;
 };
 #endif
