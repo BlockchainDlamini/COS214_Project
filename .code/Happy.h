22182a69c1ab885bc9106c1258ed5cea71a0bdff
ArisNeimanRose <129956034+ArisNeimanRose@users.noreply.github.com>
2023-10-24 21:12:47 +0200

    Update Happy.h

--- .code/Happy.h
+++ .code/Happy.h
@@ -3,8 +3,9 @@
 #include "EmotionState.h"
 class Happy : public EmotionState
 {
-     bool payBill();
-    void handleChange();
+    //bool payBill();
+    void handleChange(std::shared_ptr<Customer>);
     std::shared_ptr<Customer> getState();
+    float getTip();
 };
 #endif
