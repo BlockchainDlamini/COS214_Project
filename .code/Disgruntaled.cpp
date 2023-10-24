912dad6b9fb9facef47fe38dc3bf711a892a61a3
ArisNeimanRose <129956034+ArisNeimanRose@users.noreply.github.com>
2023-10-24 21:10:39 +0200

    Added the Emotion State hierarchy cpp files

---
+++ .code/Disgruntaled.cpp
@@ -0,0 +1,28 @@
+#include "Disgruntaled.h"
+float Disgruntaled::getTip()
+{
+    tip = 0;
+    return tip;
+}
+void Disgruntaled::handleChange(std::shared_ptr<Customer> context)
+{
+    std::shared_ptr<EmotionState> ptr;
+    int num = getRandomNumber();
+    if (num <= 25)
+    {
+        ptr = make_shared<Satisfied>();
+    }
+    else if (num > 25 && num <= 50)
+    {
+        ptr = make_shared<Disgruntaled>();
+    }
+    else if (num > 50 && num <= 75)
+    {
+        ptr = make_shared<Frustrated>();
+    }
+    else
+    {
+        ptr = make_shared<Happy>();
+    }
+    context->setEmotionState(ptr);
+}
