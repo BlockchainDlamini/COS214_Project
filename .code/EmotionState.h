f54a08d7d781e95db04af265778ca5d034d67ce6
ArisNeimanRose <129956034+ArisNeimanRose@users.noreply.github.com>
2023-10-24 20:13:29 +0200

    Update EmotionState.h
    Added constructor and fixed handleChange function, it now has shared_ptr for the Customer class

--- .code/EmotionState.h
+++ .code/EmotionState.h
@@ -1,15 +1,16 @@
 #ifndef EmotionState_H
 #define EmotionState_H
 #include "Customer.h"
-class Customer;
+
 class EmotionState
 {
 private:
     std::shared_ptr<Customer> context;
 
 public:
+    EmotionState(){};
     virtual bool payBill() = 0;
-    virtual void handleChange() = 0;
+    virtual void handleChange(std::shared_ptr<Customer>) = 0;
     virtual std::shared_ptr<Customer> getState() = 0;
 };
 #endif
