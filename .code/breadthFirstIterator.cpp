c78a472103bb3014e40cbcf76918aa17178f45f6
JessBloem <133756137+JessBloem@users.noreply.github.com>
2023-10-26 15:08:59 +0200

    first version of breadth iterator

--- .code/breadthFirstIterator.cpp
+++ .code/breadthFirstIterator.cpp
@@ -7,15 +7,41 @@ breadthFirstIterator::breadthFirstIterator(std::shared_ptr<floorComponent> head)
 
 void breadthFirstIterator::flattenTree(std::shared_ptr<floorComponent> currNode)
 {
-    for (int i = 0; i < currNode->getNumChildren(); i++)
+    int numLevels = height(head);
+    for (int i = 0; i < numLevels; i++)
     {
-        flattenedTree.push(currNode->getChild(i));
+        pushCurrentLevel(head, i);
     }
-    for (int i = 0; i < currNode->getNumChildren(); i++)
+}
+
+void breadthFirstIterator::pushCurrentLevel(std::shared_ptr<floorComponent> hd, int level)
+{
+    if(level==0)
+        flattenedTree.push(hd);
+    else
     {
-        flattenTree(currNode->getChild(i));
+        for (int i = 0; i < hd->getNumChildren(); i++)
+        {
+            pushCurrentLevel(hd->getChild(i), level-1);
+        }
     }
-    
+}
+
+int breadthFirstIterator::height(std::shared_ptr<floorComponent> curr)
+{
+    if (curr == nullptr)
+        return -1;
+    else {
+        // Compute the height of each subtree
+        int max = -1;
+        for (int i = 0; i <curr->getNumChildren(); i++)
+        {
+            int childHeight = height(curr->getChild(i));
+            if(childHeight>max)
+                max=childHeight;            
+        }
+        return max + 1;
+    }
 }
 
 breadthFirstIterator::~breadthFirstIterator()
