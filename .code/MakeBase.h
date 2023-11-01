b808ae5ebd3337b9a0a24dcd189b4457315a30e8
BlockchainDlamini <u22906160@tuks.co.za>
2023-11-01 12:27:34 +0200

    Kitchen: Updated MakeCommands

--- .code/MakeBase.h
+++ .code/MakeBase.h
@@ -20,13 +20,15 @@ public:
      * @brief Constructor for the MakeBase class.
      * @param kitchen A shared pointer to the Kitchen where the base is created.
      */
-    MakeBase(std::shared_ptr<Kitchen> kitchen);
+    MakeBase(std::shared_ptr<Kitchen>, std::string);
 
     /**
      * @brief Execute the MakeBase command, creating and returning a shared pointer to the FoodItem representing the base.
      * @return A shared pointer to the created FoodItem object.
      */
     std::shared_ptr<FoodItem> execute();
+private:
+    std::string size;
 };
 
 #endif
