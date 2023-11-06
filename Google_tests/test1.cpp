6d3cdf701a4fa10daac0eb079df1cb0dd60eb114
Hamza Mokiwa <131655930+CRYatUP@users.noreply.github.com>
2023-11-07 00:01:05 +0200

    Updates test1.cpp

--- Google_tests/test1.cpp
+++ Google_tests/test1.cpp
@@ -171,3 +171,189 @@ EXPECT_EQ(order3String, expectedString3);
 EXPECT_EQ(order4String, expectedString4);
 }
 */
+#include "lib/googletest/include/gtest/gtest.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+// Hamza add your own include directories
+using namespace std;
+class Emotion_State:public std::enable_shared_from_this<Emotion_State>{
+public:
+    Emotion_State();
+};
+
+Emotion_State::Emotion_State() {
+
+}
+
+class Happy_1:public Emotion_State, public std::enable_shared_from_this<Happy_1>{
+public:
+    Happy_1(){}
+};
+
+class Customer_Test
+{
+private:
+    int ID;
+    string tabID;                                                              /**< The unique identifier of the customer's order. */
+    std::shared_ptr<Emotion_State> mood = make_shared<Happy_1>();                 /**< The emotional state of the customer. */
+    //std::shared_ptr<OrderProcessState> orderProcess = make_shared<Preorder>(); /**< The current order process state. */
+    float bankAccountAmount;                                                   /**< The amount of money in the customer's bank account. */     /**< The customer's tab for keeping track of orders. */
+    int tableNum;
+    // Create an array of shared_ptr to strings for complaints
+    std::shared_ptr<std::string> complaints[10]; /**an array of shared_ptr to strings for complaints for the Customer_Test*/
+
+    // Create an array of shared_ptr to strings for happyMessages
+    std::shared_ptr<std::string> happyMessages[10]; /**an array of shared_ptr to strings for happyMessages for the Customer_Test*/
+    float total;                                    /**< The table number where the customer is seated. */
+    string name; /**<The name of the customer*/
+
+public:
+    string getName()
+    {
+        return name;
+    }
+    void setName(string name)
+    {
+        this->name = name;
+    }
+    Customer_Test(int id, float bankAmount)
+    {
+        bankAccountAmount = bankAmount;
+        ID = id;
+        tableNum = 0;
+        tabID = "No Tab";
+        for (int i = 0; i < 10; i++)
+        {
+            complaints[i] = make_shared<string>();
+            happyMessages[i] = make_shared<string>();
+        }
+        // Assign string values using the assign method
+        complaints[0]->assign("I ordered a pizza last night, and it arrived cold. I was really disappointed with the quality of the food.");
+        complaints[1]->assign("The delivery took over an hour, and when my pizza finally arrived, it was completely burnt. This is unacceptable.");
+        complaints[2]->assign("I found a hair in my pizza, and it completely ruined my dining experience. Hygiene should be a top priority.");
+        complaints[3]->assign("The pizza was very greasy, and it gave me an upset stomach. I expected better quality from your restaurant.");
+        complaints[4]->assign("I requested no onions on my pizza, but it came with onions anyway. Please pay more attention to custom orders.");
+        complaints[5]->assign("The pizza crust was undercooked and doughy. It was practically inedible.");
+        complaints[6]->assign("The online menu showed a variety of toppings, but when I ordered, I was told that some were not available. This inconsistency is frustrating.");
+        complaints[7]->assign("The portion size of the pizza was much smaller than I expected, and I felt like I didn't get my money's worth.");
+        complaints[8]->assign("The customer service was rude and unhelpful when I called to address an issue with my order.");
+        complaints[9]->assign("I dined in, and the restaurant was so noisy and crowded that it was impossible to have a conversation. It really affected my experience.");
+
+        // Assign string values to the happyMessages array
+        happyMessages[0]->assign("I just wanted to say how much I love your pizza! It's always so delicious and consistently amazing.");
+        happyMessages[1]->assign("Your delivery service is top-notch. The pizza always arrives hot and on time. Thank you for the great service.");
+        happyMessages[2]->assign("The Margherita pizza I had at your restaurant last night was heavenly. I can't stop thinking about it.");
+        happyMessages[3]->assign("Your staff is always friendly and welcoming. Dining at your restaurant feels like being part of a big, pizza-loving family.");
+        happyMessages[4]->assign("I appreciate the variety of pizza options on your menu. It's great to have so many choices to suit different tastes.");
+        happyMessages[5]->assign("The garlic knots that come with the pizza are the best I've ever had. They're a delightful addition to the meal.");
+        happyMessages[6]->assign("I love how fresh the ingredients on your pizza are. It's clear that you use high-quality, locally sourced products.");
+        happyMessages[7]->assign("The atmosphere at your restaurant is cozy and inviting. It's the perfect place to enjoy a slice or two with friends.");
+        happyMessages[8]->assign("The specialty pizzas on your menu are creative and unique. It's always a pleasure to try something new.");
+        happyMessages[9]->assign("I hosted a party and ordered a large pizza catering package from your restaurant. Everyone raved about the delicious pizzas. You made my event a hit!");
+    }
+    Customer_Test(std::shared_ptr<Emotion_State> mood, float bankAccountAmount)
+    {
+        this->mood = mood;
+        this->bankAccountAmount = bankAccountAmount;
+    }
+    Customer_Test(float bankAccount)
+    {
+        bankAccountAmount = bankAccount;
+    }
+
+
+    void payBill(char c, float t, bool check) // returns true if the customer has either paid or added the bill to a tab
+    {
+        if (check == true)
+        {
+            //leave();
+        }
+
+        //changedOrderProcessState(); // take the state to dinnerdone from orderreceived
+        setTotal(t);
+        if (hasBill == true)
+        {
+            if (c == 'P' || c == 'p')
+            {
+                if (bankAccountAmount < total)
+                {
+                    cout << "Customer_Test cannot pay the bill, insufficient funds" << endl;
+                }
+                else
+                {
+                    float tip = total + (total);
+                    bankAccountAmount = bankAccountAmount - (total + tip);
+
+                    cout << endl;
+                    cout << "Customer_Test busy paying";
+                    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+                    cout << ".";
+                    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+                    cout << ".";
+                    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+                    cout << ".";
+                    cout << endl;
+
+                    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+                    cout << "\033[45;97mCustomer_Test has paid the bill, and the payment was accepted\033[0m" << endl;
+                    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+                }
+            }
+            else if (c == 'T' || c == 't' == true) // has a tab and wants to add to tab
+            {
+
+                cout << "Order has been added to the tab" << endl;
+            }
+            else if (c == 'T' || c == 't' == false) // doesn't have a tab but wants to add
+            {
+                //cout << "The Customer_Test [" + to_string(myID) + "] is not a loyal customer" << endl;
+                cout << "The Customer_Test cannot add the order to the tab, because they aren't loyal. Customer_Test will pay the bill";
+                payBill('P', t, check);
+            }
+            else
+            {
+                cout << "Incorrect input, Payment Declined" << endl;
+                cout << "Customer_Test will pay the bill, normally";
+                payBill('P', t, check);
+            }
+        }
+        else
+        {
+            cout << "Customer_Test has not received the bill from the waiter" << endl;
+        }
+        // leave();
+    }
+
+
+    void beSeated(int table)
+    {
+        if (table > 0)
+        {
+            tableNum = table;
+        }
+    }
+    int getTableNum()
+    {
+        return tableNum;
+    }
+
+    string getEmotion(){
+        return "Happy_1";
+    }
+    std::string printCustomer_Test()
+    {
+        if (tableNum == 0)
+        {
+            std::string output = "Customer_Test Name: [" + name + "] Mood: [" + getEmotion() +"]";
+            return output;
+        }
+        else
+        {
+            std::string output = "Customer_Test Name: [" + name + "] Mood: [" + getEmotion()+"]";
+            return output;
+        }
+    }
+    int getID()
+    {
